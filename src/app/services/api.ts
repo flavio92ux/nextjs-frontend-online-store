5bab33cbc25dcd332f8368dbd8c869d62e0e7c8b
Flavio F <flavio.filho@hotmail.com>
2023-08-12 20:40:31 -0300

    Criando requisicoes de api

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,10 +1,21 @@
-interface AllCategories {
-  id: string;
-  name: string
-}
+import { AllCategories } from "../interfaces/allCategories"
+import { ProductsFromCategoryAndQuery } from "../interfaces/productsFromCategoryAndQuery"
 
 export async function getCategories(): Promise<AllCategories[]> {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+
+  const data = response.json()
+
+  return data
+}
+
+export async function getProductsFromCategoryAndQuery(categoryId: string, query: string): Promise<ProductsFromCategoryAndQuery> {
+  const urlParams = new URLSearchParams({
+    category: categoryId,
+    q: query
+  })
+
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?' + urlParams)
 
   const data = response.json()
 

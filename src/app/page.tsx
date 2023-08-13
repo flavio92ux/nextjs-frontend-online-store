d85e33885094d641a38c98980146e05c86a80eb0
Flavio F <flavio.filho@hotmail.com>
2023-08-12 21:24:03 -0300

    Requisitando categorias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import { GroupForm } from './components/GroupForm'
+import { getCategories } from './services/api'
+import { AllCategories } from './interfaces/allCategories';
 
-export default function Home() {
-  return <GroupForm />
+// export async function getServerSideProps() {
+//   const data = await getCategories()
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
+
+export default async function Home() {
+  const data: AllCategories[] = await getCategories()
+  return (
+    <div>
+      <ul>
+        {data.map((item: AllCategories) => <li key={item.id}>{item.name}</li>)}
+      </ul>
+      <GroupForm />
+    </div>
+  )
 }

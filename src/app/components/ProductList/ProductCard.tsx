85496237be8641a57d4e77faed41404a80c31c44
Flavio F <flavio.win@gmail.com>
2023-08-26 16:31:58 -0300

    Adicionando botão adicionoar ao carrinho

--- src/app/components/ProductList/ProductCard.tsx
+++ src/app/components/ProductList/ProductCard.tsx
@@ -15,27 +15,33 @@ export function ProductCard({ product }: IProductCard) {
   }
 
   return (
-    <button type='button' onClick={handleClick}>
-      <div className='w-[360px] h-[367px] bg-white rounded-[20px] shadow m-4'>
+    <div className='w-[360px] h-[367px] bg-white rounded-[20px] shadow m-4'>
+      <button type='button' onClick={handleClick}>
         <div className='p-[25px] justify-center items-center'>
           <p>{product.title}</p>
         </div>
+      </button>
 
-        <div className='py-1 justify-center items-center flex'>
-          <Image
-            width={150}
-            height={150}
-            src={product.thumbnail}
-            alt={product.title}
-          />
-        </div>
+      <div className='py-1 justify-center items-center flex'>
+        <Image
+          width={150}
+          height={150}
+          src={product.thumbnail}
+          alt={product.title}
+        />
+      </div>
 
-        <div className='flex p-[25px] justify-center items-center'>
-          <div className='text-slate-500 text-base font-normal leading-[23px] tracking-wide'>
-            {product.price}
-          </div>
+      <div className='flex p-[25px] justify-center items-center'>
+        <div className='text-slate-500 text-base font-normal leading-[23px] tracking-wide'>
+          {product.price}
         </div>
       </div>
-    </button>
+
+      <div className='flex justify-center'>
+        <button className='border rounded px-6 py-1'>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </div>
+    </div>
   )
 }
